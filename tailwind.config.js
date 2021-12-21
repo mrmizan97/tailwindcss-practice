64eaedfb3f19a65b7f146f35ec25573618e085e7
mizangitlab <mrmizanbd97@gmail.com>
2021-12-21 11:42:36 +0600

    task added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.{html,js}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
+    colors: {
+      blue: {
+        light: "#f5f5f5",
+        DEFAULT:"#f6f6",
+        dark:"#a5a5a5",
+      },
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    screens: {
+      sm: "540px",
+      lg: "1000px",
+      "4k": "2560px",
+    },
+
     extend: {
-      colors: {
-        myColor: {
-          300: "#d5d5d5",
-        },
+      fontFamily: {
+        sans: ["Robot", ...defaultTheme.fontFamily.sans],
       },
     },
   },
